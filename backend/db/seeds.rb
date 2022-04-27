998aaf6a9ed24ef67603983bdc042b0eec396881
Daniel Kalman <kalman.d@ip-192-168-1-214.eu-west-1.compute.internal>
2022-04-28 00:02:13 +0300

    Add item creation to seed.rb file

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Item(id: integer, title: string, slug: string, description: string, image: string, favorites_count: integer, user_id: integer, created_at: datetime, updated_at: datetime, tag_list: )
