7b3909a733dd5a3a06191e887952f1dc47a6ac17
Daniel Kalman <daniel.kman@gmail.com>
2022-05-19 20:37:15 +0300

    Fix agent (#10)
    * Fix agent cond
    
    * Add api
    
    * wip
    
    * wip
    
    * Add title filter
    
    * Change scope to like
    
    Co-authored-by: Daniel Kalman <kalman.d@ip-192-168-1-213.eu-west-1.compute.internal>

--- backend/app/models/item.rb
+++ backend/app/models/item.rb
@@ -7,6 +7,7 @@ class Item < ApplicationRecord
 
   scope :sellered_by, ->(username) { where(user: User.where(username: username)) }
   scope :favorited_by, ->(username) { joins(:favorites).where(favorites: { user: User.where(username: username) }) }
+  scope :with_title, ->(title) { where("title like ?", "#{title}%") }
 
   acts_as_taggable
 
