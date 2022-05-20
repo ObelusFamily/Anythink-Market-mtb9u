e3befa71ef9966609bdfce2dada61e5598ca22a8
Daniel Kalman <daniel.kman@gmail.com>
2022-05-20 16:14:12 +0300

    Added search box (#11)
    Co-authored-by: Daniel Kalman <kalman.d@ip-192-168-1-213.eu-west-1.compute.internal>

--- backend/app/models/item.rb
+++ backend/app/models/item.rb
@@ -7,7 +7,7 @@ class Item < ApplicationRecord
 
   scope :sellered_by, ->(username) { where(user: User.where(username: username)) }
   scope :favorited_by, ->(username) { joins(:favorites).where(favorites: { user: User.where(username: username) }) }
-  scope :with_title, ->(title) { where("title like ?", "#{title}%") }
+  scope :with_title, ->(title) { where("title ilike ?", "%#{title}%") }
 
   acts_as_taggable
 
