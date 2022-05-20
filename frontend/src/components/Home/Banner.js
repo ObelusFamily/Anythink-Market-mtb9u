307b2c45a18e30182fa11d47b6bd93db854a2bd9
Daniel Kalman <daniel.kman@gmail.com>
2022-05-20 17:25:31 +0300

    Hide search (#13)
    * Hide search
    
    * WIP
    
    * Format
    
    Co-authored-by: Daniel Kalman <kalman.d@ip-192-168-1-213.eu-west-1.compute.internal>

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,15 +1,18 @@
-import React from "react";
+import { React, useState } from "react";
 import logo from "../../imgs/logo.png";
 import Search from "../search/search";
 
 const Banner = (props) => {
+  const [visible, setVisible] = useState(false);
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div>
-          <span id="get-part">A place to get</span>
-          <Search onSearch={props.onSearch} />
+          <span id="get-part" onClick={(e) => setVisible(!visible)}>
+            A place to get
+          </span>
+          <Search visible={visible} onSearch={props.onSearch} />
           <span> the cool stuff.</span>
         </div>
       </div>
